e6fbca1a38df6d97c9721811a67558e99c5bb4dc
Masa <masa@simasasolutions.com>
2019-01-08 14:08:57 +0200

    Added ignore lists

--- src/main/java/fi/matiaspaavilainen/masuitechat/bungee/channels/Global.java
+++ src/main/java/fi/matiaspaavilainen/masuitechat/bungee/channels/Global.java
@@ -2,10 +2,22 @@ package fi.matiaspaavilainen.masuitechat.bungee.channels;
 
 import fi.matiaspaavilainen.masuitechat.bungee.Utilities;
 import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class Global {
+
+    public static List<UUID> ignores = new ArrayList<>();
     public static void sendMessage(ProxiedPlayer p, String msg) {
-        ProxyServer.getInstance().broadcast(Utilities.chatFormat(p, msg, "global"));
+        BaseComponent[] txt = Utilities.chatFormat(p, msg, "global");
+        for (ProxiedPlayer player : ProxyServer.getInstance().getPlayers()) {
+            if(!ignores.contains(player.getUniqueId())){
+                player.sendMessage(txt);
+            }
+        }
     }
 }
--- src/main/java/fi/matiaspaavilainen/masuitechat/bungee/channels/Server.java
+++ src/main/java/fi/matiaspaavilainen/masuitechat/bungee/channels/Server.java
@@ -4,10 +4,19 @@ import fi.matiaspaavilainen.masuitechat.bungee.Utilities;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class Server {
+
+    public static List<UUID> ignores = new ArrayList<>();
+
     public static void sendMessage(ProxiedPlayer p, String msg) {
-        for (ProxiedPlayer players : ProxyServer.getInstance().getServerInfo(p.getServer().getInfo().getName()).getPlayers()) {
-            players.sendMessage(Utilities.chatFormat(p, msg,"server"));
+        for (ProxiedPlayer player : ProxyServer.getInstance().getServerInfo(p.getServer().getInfo().getName()).getPlayers()) {
+            if (!ignores.contains(player.getUniqueId())) {
+                player.sendMessage(Utilities.chatFormat(p, msg, "server"));
+            }
         }
     }
 }
