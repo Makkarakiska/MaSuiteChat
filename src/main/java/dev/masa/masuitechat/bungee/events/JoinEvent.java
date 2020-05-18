989393456a7829f981a137d101e109c1e1671b15
Matias Paavilainen <matias@paavilainen.net>
2020-05-18 09:50:30 +0300

    Fixed server prefix on events

--- src/main/java/dev/masa/masuitechat/bungee/events/JoinEvent.java
+++ src/main/java/dev/masa/masuitechat/bungee/events/JoinEvent.java
@@ -15,11 +15,14 @@ public class JoinEvent implements Listener {
 
     @EventHandler
     public void onJoin(PostLoginEvent e) {
+        String server = plugin.config.load("chat", "chat.yml").getString("channels." + e.getPlayer().getServer().getInfo().getName().toLowerCase() + ".prefix");
         if (plugin.config.load("chat", "messages.yml").getBoolean("connection-message.enabled")) {
+
             plugin.utils.broadcast(plugin.config.load("chat", "messages.yml")
                     .getString("connection-message.join")
                     .replace("%player%", e.getPlayer().getName())
                     .replace("%nickname%", e.getPlayer().getDisplayName())
+                    .replace("%server%", server)
             );
         }
         MaSuiteChat.players.put(e.getPlayer().getUniqueId(), "global");
@@ -27,14 +30,19 @@ public class JoinEvent implements Listener {
         if (plugin.config.load("chat", "messages.yml").getBoolean("first-join.enabled"))
             if (plugin.getApi().getPlayerService().getPlayer(e.getPlayer().getUniqueId()) == null) {
                 plugin.utils.broadcast(plugin.config.load("chat", "messages.yml")
-                        .getString("first-join.message").replace("%player%", e.getPlayer().getName()));
+                        .getString("first-join.message")
+                        .replace("%player%", e.getPlayer().getName())
+                        .replace("%server%", server)
+                );
             }
 
         if (plugin.config.load("chat", "messages.yml").getBoolean("motd.enabled")) {
             plugin.formator.sendMessage(e.getPlayer(), plugin.config.load("chat", "messages.yml")
                     .getString("motd.message")
                     .replace("%player%", e.getPlayer().getName())
-                    .replace("%nickname%", e.getPlayer().getDisplayName())
+                    .replace("%nickname%", e.getPlayer().getDisplayName()
+                            .replace("%server%", server)
+                    )
             );
         }
     }
--- src/main/java/dev/masa/masuitechat/bungee/events/SwitchEvent.java
+++ src/main/java/dev/masa/masuitechat/bungee/events/SwitchEvent.java
@@ -17,11 +17,12 @@ public class SwitchEvent implements Listener {
     public void onConnect(ServerConnectEvent e) {
         if (!(e.getPlayer().getServer() == null) && (e.getTarget() != null) && (!e.getPlayer().getServer().getInfo().equals(e.getTarget()))) {
             if (plugin.config.load("chat", "messages.yml").getBoolean("switch-message.enabled")) {
+                String server = plugin.config.load("chat", "chat.yml").getString("channels." + e.getPlayer().getServer().getInfo().getName().toLowerCase() + ".prefix");
                 plugin.utils.broadcast(plugin.config.load("chat", "messages.yml")
                         .getString("switch-message.message")
                         .replace("%player%", e.getPlayer().getName())
                         .replace("%nickname%", e.getPlayer().getDisplayName())
-                        .replace("%server%", e.getTarget().getName())
+                        .replace("%server%", server)
                 );
             }
             MaSuiteChat.players.put(e.getPlayer().getUniqueId(), plugin.config.load("chat", "chat.yml")
